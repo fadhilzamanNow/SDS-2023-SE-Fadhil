01838fae80a8a848ef44cc631a5e920e5352c8b1
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2023-09-23 00:56:33 +0700

    pt3

---
+++ tugas-camp-1/luasLingkaran.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func luasLingkaran(radius float64) {
+	fmt.Println("Radius : ", radius)
+	fmt.Println("Area   : ", 3.14*radius*radius, "m^2")
+
+}
+
+func main() {
+	fmt.Println("Menghitung luas lingkaran")
+	luasLingkaran(4)
+}
