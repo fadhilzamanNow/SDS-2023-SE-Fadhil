78b86ec266b4730e13ba6bbbad767660106d0f62
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2023-10-07 16:01:04 +0700

    update-2

--- uts-sds-muhammad_ilham_isfadhillah/main.go
+++ uts-sds-muhammad_ilham_isfadhillah/main.go
@@ -15,7 +15,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("UTS SDS")
 	})
 
 	app.Listen(":3000")
